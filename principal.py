99ee8e1175999af16c661e57cb49aa448b25dec2
Diane Silva <dianesilva5030@gmail.com>
2020-11-08 20:42:21 -0300

    trabalho desenv sistemas ac05

---
+++ principal.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/detalhe/<string:time>")
+def detalhe(time):
+    return render_template("detalhe_" + time + ".html") 
+
+if __name__ == "__main__":
+    app.run(debug=True)
